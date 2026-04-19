f42be9186b129521da5a98101336d79d5b964765
aliimranrauf-oss <aliimranrauf@gmail.com>
2026-04-19 12:18:21 +0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -24,6 +24,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
       },
     },
 
+    {
+      url: 'https://www.makemystore.online/pk/online-store-pakistan',
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.9,
+      alternates: {
+        languages: {
+          en: 'https://www.makemystore.online/',
+          ur: 'https://www.makemystore.online/pk/online-store-pakistan',
+        },
+      },
+    },
+
     // Service Pages (0.8)
     {
       url: 'https://www.makemystore.online/tools',
