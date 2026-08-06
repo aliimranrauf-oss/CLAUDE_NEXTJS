7fcd8054afd701b8e9d81089feb5a79207989434
aliimranrauf-oss <aliimranrauf@gmail.com>
2026-08-06 15:41:11 +0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,6 +43,7 @@ export default function Navbar() {
 
   return (
     <nav
+      dir="ltr"
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
         scrolled ? 'glass shadow-lg' : 'bg-transparent'
       }`}
