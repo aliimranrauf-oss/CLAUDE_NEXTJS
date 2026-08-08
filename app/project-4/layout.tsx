62b4f8b4981a2560b34cd6aa2dbe48cab00786a5
aliimranrauf-oss <aliimranrauf@gmail.com>
2026-08-08 14:47:18 +0500

    Update layout.tsx

--- app/project-4/layout.tsx
+++ app/project-4/layout.tsx
@@ -3,6 +3,7 @@ import { newsreader, plusJakarta, reemKufi } from './fonts'
 import './portfolio.css'
 import Header from './_components/Header'
 import Footer from './_components/Footer'
+import TemplateActionBar from './_components/TemplateActionBar'
 import { SiteSettingsProvider } from './_context/SiteSettingsContext'
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function Project4Layout({ children }: { children: React.ReactNode
         <Header />
         <main>{children}</main>
         <Footer />
+        <TemplateActionBar />
       </SiteSettingsProvider>
     </div>
   )
