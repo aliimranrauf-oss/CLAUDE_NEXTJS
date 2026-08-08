27f78a585aa59db1afb40ef9b71107890c3fe280
aliimranrauf-oss <aliimranrauf@gmail.com>
2026-08-08 14:46:20 +0500

    Update layout.tsx

--- app/project-2/layout.tsx
+++ app/project-2/layout.tsx
@@ -3,6 +3,7 @@ import { playfair, instrumentSerif, inter } from './fonts'
 import './portfolio.css'
 import Header from './_components/Header'
 import Footer from './_components/Footer'
+import TemplateActionBar from './_components/TemplateActionBar'
 import { SiteSettingsProvider } from './_context/SiteSettingsContext'
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function Project2Layout({ children }: { children: React.ReactNode
         <Header />
         <main>{children}</main>
         <Footer />
+        <TemplateActionBar />
       </SiteSettingsProvider>
     </div>
   )
